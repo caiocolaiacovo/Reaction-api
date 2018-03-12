56ef66a10d2324731cda6a6b703e1b0f225339a5
caiobefore <caio.colaiacovo@before.com.br>
2018-03-12 13:11:05 -0400

    Creating middleware

--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -34,7 +35,7 @@ namespace Reaction_api
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc();
+            app.Run(context => context.Response.WriteAsync("Olá mundo!"));
         }
     }
 }
